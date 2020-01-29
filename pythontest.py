55fd34cc66372b2334c50b761a8f7a7cc0462d4b
JayLee <ralph.carro.co>
2020-01-29 13:38:12 +0800

    Upgrade from printing, to math.

--- pythontest.py
+++ pythontest.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+# Simple Math
+
+number1 = input("Enter 1st number:")
+number2 = input("Enter 2nd number:")
+
+while True:
+    if number1.isdigit() and number2.isdigit():
+        x = int(number1) + int(number2)
+        print("1st number + 2nd number =", x)
+    else:
+        print("Please input a number.")
+    break
