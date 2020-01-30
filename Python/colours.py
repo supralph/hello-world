5d1433e29f54e3d2694f03b5c7e20348c810c48f
JayLee <ralph.carro.co>
2020-01-30 14:50:21 +0800

    import values and data from other files

--- Python/colours.py
+++ Python/colours.py
@@ -4,3 +4,11 @@ i = 0
 while i < len(colours):
     print("When I was %d, my favourite colour was %s" % (i, colours[i]))
     i += 1
+
+def main():
+    print("Yes, \"__name__\" of file is equals to \"__main__\".")
+
+if __name__ == '__main__':
+    main()
+
+import user_key
