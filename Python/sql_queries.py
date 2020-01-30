18dd5d918e14fa51f695b6cf7ee996da58736635
JayLee <ralph.carro.co>
2020-01-30 15:17:32 +0800

    create sql query file

---
+++ Python/sql_queries.py
@@ -0,0 +1,19 @@
+tickets_mart_sql = '''
+    SELECT
+        *
+    FROM tickets
+    WHERE
+        YEAR(created_at) >= 2020;
+    '''
+
+abc_auctions = '''
+    SELECT
+        id,
+        DATE(DATE_ADD(created_at, interval 8 hour)) as created_at,
+        title,
+        reserve_price
+    FROM auctions
+    WHERE
+        deleted_at is null AND 
+        country_id = 1;
+    '''
---
+++ Python/tickets_mart_creation.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.engine.url import URL
+import database_key #log-in credentials for databases
+import sql_queries #sql query to extract out from database
+
+carro_wholesale_db_url = URL(**(database_key.carro_wholesale_db))
+jay_test_db_url = URL(**(database_key.jay_test_db))
+
+engine_out = sqlalchemy.create_engine(carro_wholesale_db_url) # extract data from database
+engine_in = sqlalchemy.create_engine(jay_test_db_url) #dump data into database
+
+query = sql_queries.tickets_mart_sql
+
+df = pd.read_sql_query(query, engine_out)
+
+df.to_sql(
+    name = 'test_table',
+    con = engine_in,
+    index = False,
+    if_exists = 'replace'
+)
