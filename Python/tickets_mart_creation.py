9066c9443c67d52e63fb6d23ad0e54ffb95d4a99
JayLee <ralph.carro.co>
2020-01-30 15:25:39 +0800

    add comments to script

--- Python/tickets_mart_creation.py
+++ Python/tickets_mart_creation.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import sqlalchemy
 from sqlalchemy.engine.url import URL
-import database_key #log-in credentials for databases
-import sql_queries #sql query to extract out from database
+import database_key # log-in credentials for databases
+import sql_queries # sql query to extract out from database
 
 carro_wholesale_db_url = URL(**(database_key.carro_wholesale_db))
 jay_test_db_url = URL(**(database_key.jay_test_db))
 
 engine_out = sqlalchemy.create_engine(carro_wholesale_db_url) # extract data from database
-engine_in = sqlalchemy.create_engine(jay_test_db_url) #dump data into database
+engine_in = sqlalchemy.create_engine(jay_test_db_url) # dump data into database
 
 query = sql_queries.tickets_mart_sql
 
