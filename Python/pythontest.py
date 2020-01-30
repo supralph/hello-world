efac9757adedd9584e0d57f5e0b7f53389910799
JayLee <ralph.carro.co>
2020-01-30 11:20:49 +0800

    Jualo Rails DB info

--- Python/pythontest.py
+++ Python/pythontest.py
@@ -1,14 +1,24 @@
-import configparser
+# Simple Math
 
-config = configparser.ConfigParser()
-config.read('key.ini')
+number1 = input("Enter 1st number:")
+number2 = input("Enter 2nd number:")
 
-host = config['jualo_slave_db']['hostname']
-user = config['jualo_slave_db']['user']
-password = config['jualo_slave_db']['password']
-db = config['jualo_slave_db']['db']
+while True:
+    if number1.isdigit() and number2.isdigit():
+        x = int(number1) + int(number2)
+        print("1st number + 2nd number =", x)
+    else:
+        print("Please input a number.")
+    break
 
-print(f'Host: {host}')
-print(f'User: {user}')
-print(f'Password: {password}')
-print(f'Database: {db}')
+
+number3 = input("Enter 3rd number:")
+number4 = input("Enter 4th number:")
+
+while True:
+    if number3.isdigit() and number4.isdigit():
+        y = int(number3) * int(number4)
+        print("3rd number + 4th number =", y)
+    else:
+        print("Please input a number.")
+    break
