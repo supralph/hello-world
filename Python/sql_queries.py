db9cfdccad00e7fef839ec4a55a729cc63d430e1
JayLee <ralph.carro.co>
2020-01-31 16:46:55 +0800

    add comments to script

--- Python/sql_queries.py
+++ Python/sql_queries.py
@@ -6,14 +6,51 @@ tickets_mart_sql = '''
         YEAR(created_at) >= 2019;
     '''
 
-abc_auctions = '''
+carrsg_auction_inventories = '''
     SELECT
-        id,
-        DATE(DATE_ADD(created_at, interval 8 hour)) as created_at,
-        title,
-        reserve_price
-    FROM auctions
+	make AS car_make,
+	model AS car_model,
+	submodel AS car_submodel,
+	a.title AS car_name,
+	manufacture_year AS car_manufacture_year,
+	JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.original_registration_date")) AS car_registration_date,
+	ROUND((DATEDIFF(now(),JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.original_registration_date"))) / 365), 1) AS car_age_years,
+	CASE 
+	WHEN i.type = "new" THEN 1 
+	WHEN i.type = "used" THEN 0 
+	END AS car_new_used,
+	color AS car_colour,
+	engine_capacity AS car_engine_displacement,
+	fuel_type AS car_fuel_type,
+	ROUND(JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.omv")), 0) AS car_omv,
+	ROUND(JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.arf")), 0) AS car_arf,
+	ROUND(JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.coe_qp")), 0) AS car_coe_qp,
+	IFNULL(IF(JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.coe_category")) = "null", NULL, NULL), CASE WHEN engine_capacity > 1600 THEN "B" WHEN engine_capacity <= 1600 THEN "A" END) AS car_coe,
+	ROUND(JSON_UNQUOTE(JSON_EXTRACT(i.registration_data, "$.transfer_count")), 0) AS car_owner_count,
+	COUNT(DISTINCT a.id) AS auction_count,
+	ROUND(AVG(TIMESTAMPDIFF(HOUR, a.time_start, a.time_end)), 0) AS auction_duration_hours,
+	COUNT(DISTINCT b.id) AS bid_count,
+	ROUND(AVG(b.amount), 0) AS avg_bid_price,
+	ROUND(AVG(bb.amount), 0) AS highest_bid_price,
+	i.country_id 
+    FROM
+	inventories AS i
+    LEFT JOIN
+	auction_inventory AS ai
+	ON ai.inventory_id = i.id
+    LEFT JOIN
+	auctions AS a 
+	ON ai.auction_id = a.id
+    LEFT JOIN
+	bids AS b
+	ON a.id = b.auction_id
+    LEFT JOIN	
+	bids AS bb
+	ON a.highest_bid_id = bb.id
     WHERE
-        deleted_at is null AND 
-        country_id = 1;
-    '''
+	i.deleted_at IS NULL AND
+	a.deleted_at IS NULL AND
+	b.id IS NOT NULL
+    GROUP BY
+	i.id;
+'''
--- Python/tickets_mart_creation.py
+++ Python/tickets_mart_creation.py
@@ -1,8 +1,20 @@
+import configparser
+import logging
 import pandas as pd
 import sqlalchemy
 from sqlalchemy.engine.url import URL
 import database_key # log-in credentials for databases
 import sql_queries # sql query to extract out from database
+
+logging.basicConfig(
+    filename='script.log',
+    filemode= 'a',
+    format = '%(asctime)s :: %(levelname)s :: %(funcName)s :: %(message)s',
+    level = logging.INFO
+)
+logging.info("----------------------------------------------")
+logging.info("Extracting data from Carro Wholesale database.")
+
 
 carro_wholesale_db_url = URL(**(database_key.carro_wholesale_db))
 jay_test_db_url = URL(**(database_key.jay_test_db))
@@ -20,6 +32,6 @@ df.to_sql(
     index = False,
     if_exists = 'replace'
 )
-
-print("Data exported into local database.")
+logging.info("There are {} tickets".format(len(df)))
+logging.info("Data has been extracted to Jay Localhost database.")
 
