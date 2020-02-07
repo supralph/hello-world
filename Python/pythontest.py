778100825f3da3ce5458692794cb3e1ad9187e8f
JayLee <ralph.carro.co>
2020-02-07 10:12:42 +0800

    MED

--- Python/pythontest.py
+++ Python/pythontest.py
@@ -32,3 +32,13 @@ df = pg.get_result("SELECT * FROM xxx")
 pg.create_table_from_query("SELECT * FROM XXX LIMIT 100", dataset="warehouse", table_name="inventory")
 
 abc test NEW
+
+
+Publisher Subscriber
+
+ticket change state >> publish message to a topic >>
+read message from topic >> stream to table
+
+
+google tag manager >> webhook http post request with json payload
+
